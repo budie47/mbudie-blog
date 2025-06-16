ef5d5354638ceb2b8ecad62b1eb9dae6d9709561
budie <budiebasri@gmail.com>
2025-06-16 22:12:05 +0800

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         M.Budie
       </h1>
       <p className="mb-4">
-        {`🚀 Cloud & VDI Enthusiast | Aspiring AI Engineer | Gunpla Builder
+        {`🚀 Cloud & VDI Enthusiast | Aspiring AI Engineer | Gunpla Builder Test blog on new repo
           Currently working at Deloitte Malaysia, I specialize in VDI Technology, AWS, and Azure Cloud solutions. Passionate about cloud computing, virtualization, and automation, I’m on a journey to become an AI Engineer, blending cloud and AI to drive intelligent solutions.
           Beyond tech, I’m a Gunpla enthusiast, building and customizing Gundam plastic models—stay tuned for reviews and build experiences! I also enjoy gaming in my free time, always exploring new worlds and challenges.
           🔹 Cloud | Virtualization | AI & ML | Gunpla & Gaming`}
